e015cc8319976014abed65a8254f3785d85a3463
hyxuu <hyxuu05@gmail.com>
2025-11-24 13:33:29 -0500

    practica

---
+++ src/vuelos/vuelo.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('vuelos')
+export class Vuelo {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  origen: string;
+
+  @Column()
+  destino: string;
+
+  @Column()
+  fecha: Date;
+
+  @Column()
+  precio: number;
+
+  @Column()
+  asientosDisponibles: number;
+
+  @Column()
+  estado: boolean;
+}
